aeb4b827dc476b59a229d492444109fc43653b02
Shamim Ahammad Rasel <87210321+rasel-33@users.noreply.github.com>
2022-11-06 01:38:28 +0600

    Update 10679 - I Love Strings!!.cpp

--- UVa/10679 - I Love Strings!!.cpp
+++ UVa/10679 - I Love Strings!!.cpp
@@ -3,150 +3,106 @@ using namespace std;
 
 #define EPS      1E-9
 #define PI       acos(-1)
-#define N        100005
+#define N        1000006
 #define M        1000000007
 #define fast     ios::sync_with_stdio();cin.tie(0)
 #define ll       long long
 #define endl     "\n"
 #define pb       push_back
 #define base     179
+
 int dx[] = { -1,+0,+1,+0,-1,-1,+1,+1};
 int dy[] = { +0,-1,+0,+1,+1,-1,+1,-1};
 
-int trie[N][65];
-int triecnt[N];
-int strno[N];
-int cnt[N];
-int failure[N];
-int output[N];
-int root = 0;
-int node;
-
 int getINT(char ch){
-    if(ch>='0' && ch<='9') return ch-'0';
-    else if(ch>='A' && ch<='Z') return ch - 'A' + 10;
+    if(ch >= '0' && ch <= '9') return ch - '0';
+    else if(ch >= 'A' && ch <= 'Z') return ch - 'A' + 10;
     else return ch - 'a' + 36;
 }
 
-void insert(string& s, int strNo){
-    int cur = root;
-    for(int i=0;i<s.size();i++){
-        int id = getINT(s[i]);
-        if(trie[cur][id]==-1)
-            trie[cur][id] = ++node;
-        cur = trie[cur][id];
+//aho corasick dictionary link
+int trie[N][62], failure[N], sz = 1, str[N];
+vector<int> endNode[N];
+
+void insert(string s, int id){
+    int cur = 0;
+    for(int i = 0; i < s.size(); i++){
+        int c = getINT(s[i]);
+        if(trie[cur][c] == 0) trie[cur][c] = sz++;
+        cur = trie[cur][c];
     }
-    strno[cur] = strNo;
+    endNode[cur].pb(id);
 }
 
-void build_failure(){
+void buildFailure(){
     queue<int> q;
-    for(int i = 0; i < 65; i++){
-        if(trie[root][i] != -1){
-            q.push(trie[root][i]);
-            failure[trie[root][i]] = root;
+    for(int i = 0; i < 62; i++){
+        if(trie[0][i]){
+            q.push(trie[0][i]);
+            failure[trie[0][i]] = 0;
         }
     }
     while(!q.empty()){
         int u = q.front();
         q.pop();
-        for(int i = 0; i < 65; i++){
-            if(trie[u][i] != -1){
-                int v = trie[u][i];
-                q.push(v);
-                int f = failure[u];
-                while(f != root && trie[f][i] == -1){
-                    f = failure[f];
-                }
-                if(trie[f][i] != -1){
-                    f = trie[f][i];
-                }
-                failure[v] = f;
-                output[v] |= output[f];
+        for(int i = 0; i < 62; i++){
+            if(trie[u][i]){
+                q.push(trie[u][i]);
+                int v = failure[u];
+                while(v && trie[v][i] == 0) v = failure[v];
+                failure[trie[u][i]] = trie[v][i];
             }
-        }
-        
-    }
-}
-
-
-void search(string& s){
-    int cur = root;
-    for(int i=0;i<s.size();i++){
-        int ch = getINT(s[i]);
-        if(trie[cur][ch]!=-1){
-            cur = trie[cur][ch];
-            if(strno[cur]>-1){
-                cnt[strno[cur]]++;
-            }
-
-            int temp = output[cur];
-
-            while(temp!=-1 && temp!=root){
-                
-            }
-        }else{
-
         }
     }
 }
 
-
-
-
-void solve(int cs){
-    memset(trie, -1, sizeof(trie));
-    memset(triecnt, -1, sizeof(triecnt));
-    memset(strno, -1, sizeof(strno));
-    memset(cnt, 0, sizeof(cnt));
-    memset(failure, -1, sizeof(failure));
-    memset(output, -1, sizeof(output));
-    node = root;
-
-    string s;
-    cin >> s;
-    int n;
-    cin >> n;
-    for(int i=0;i<n;i++){
-        string x;
-        cin >> x;
-        insert(x, i+1);
+vector<int> search(string s){
+    int cur = 0;
+    vector<int> ans;
+    for(int i = 0; i < s.size(); i++){
+        int c = getINT(s[i]);
+        while(cur && trie[cur][c] == 0) cur = failure[cur];
+        cur = trie[cur][c];
+        for(int j = cur; j && endNode[j].size(); j = failure[j]){
+            for(int k = 0; k < endNode[j].size(); k++){
+                str[endNode[j][k]]=1;
+            }
+            endNode[j].clear();
+        }
     }
-    build_failure();
-    search(s);
 
-    for(int i=1;i<=node;i++){
-        cout << cnt[i] <<" ";
-    }
-    cout << endl;
-
-    bool status[node+1];
-    memset(status, 0, sizeof(status));
-
-   
-
-    
-
-    
+    return ans;
 }
 
 int main(){
-    #ifdef RASEL_DEFINED
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-        int start_time = clock();
-    #endif
     fast;
-
-    int tc = 1,cs = 1;
+    int tc;
     cin >> tc;
     while(tc--){
-        solve(cs);
-        cs++;
+        memset(trie, 0, sizeof trie);
+        memset(failure, 0, sizeof failure);
+        memset(endNode, 0, sizeof endNode);
+        memset(str, 0, sizeof str);
+        sz = 1;
+        string text;
+        cin >> text;
+        int n;
+        cin >> n;
+        for(int i = 1; i <= n; i++){
+            string s;
+            cin >> s;
+            insert(s, i);
+        }
+
+        buildFailure();
+
+        vector<int> ans = search(text);
+        for(int i = 1; i <= n; i++) {
+            if (str[i]) cout << "y" << endl;
+            else cout << "n" << endl;
+        }
+
     }
-    #ifdef RASEL_DEFINED
-        int end_time = clock();
-        printf("\nTT = %.4f\n",(end_time-start_time+0.0)/CLOCKS_PER_SEC);
-    #endif
-return 0;
+    return 0;
 }
+
